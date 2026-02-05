2002add514d550fd0a547b09c926f7884c60a7d1
SanchitaRamesh <sanchita2415kaviya@gmail.com>
2026-02-05 20:42:44 +0530

    finding even and odd number between the range 1 to 10 using a class

--- learning_git.py
+++ learning_git.py
@@ -1,8 +1,11 @@
-def even_odd(a,b):
-    for i in range(a,b):
-        if i%2==0:
-            print(f'{i} is even number')
-        else:
-            print(f"{i} is odd number")
-    #return a,b
-even_odd(1,10)
+class Numbers:
+    def even_odd(self,a,b):
+        for i in range(a,b):
+            if i%2==0:
+                print(f'{i} is even number')
+            else:
+                print(f"{i} is odd number")
+        #return a,b
+num = Numbers()
+num.even_odd(1,10)
+#even_odd(1,10)
